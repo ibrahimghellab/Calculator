1ec4de97831216d3da20ed3eea9fc5fe8a444b71
ibrahimghellab <ghellabibrahim@gmail.com>
2024-11-23 18:55:57 +0100

    Refactor main.js to improve operation handling and add equal functionality

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 import './style.css'
 
-let op=array();
+let op=new Array();
 let value="";
+let valueDisplayed="";
 let screen = document.getElementById("screen");
 
 let mult=document.getElementById("mult");
 let div=document.getElementById("div");
-let min=document.getElementById("min");
+let sub=document.getElementById("min");
 let sum=document.getElementById("sum");
 
+let equal=document.getElementById("result");
+
+let result=0;
 
 let buttonsNumbers= document.querySelectorAll(".number");
 
@@ -16,23 +20,48 @@ buttonsNumbers.forEach(element => {
     console.log(element.textContent);
 });
 
-add
 
-function mult(){
-    op.append(parseIntValue);
+function operation(ope){
+    op.push(parseInt(value));
     value="";
-    op.append("*");
+    valueDisplayed+=ope;
+    screen.textContent=valueDisplayed;
+    op.push(ope);
+    console.log(op);
 }
 
 function concatenate(text){
-    
     value+=text;
-    screen.textContent=value;
+    valueDisplayed+=text;
+    screen.textContent=valueDisplayed;
 }
 
+function equalPressed(){
+    /*switch(o){
+        case "*":
+            
+            break;
+        case "/":
+
+            break;
+        case "+":
+
+            break;
+        case "-":
+
+            break;
+        
+    }*/
+   screen.textContent+=math.evaluate(valueDisplayed);
+}
 
 buttonsNumbers.forEach(element => {
     element.addEventListener("click",()=>{concatenate(element.textContent)});
 });
 
-mult.addEventListener("click", mult); 
+mult.addEventListener("click", ()=>{operation("*")}); 
+div.addEventListener("click", ()=>{operation("/")}); 
+sub.addEventListener("click", ()=>{operation("-")}); 
+sum.addEventListener("click", ()=>{operation("+")}); 
+
+equal.addEventListener("click",()=>{equalPressed()});
